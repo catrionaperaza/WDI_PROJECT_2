79638cf961f01042bf4610d54af3e35737005d74
odwyercatriona <odwyercatriona@gmail.com>
2017-10-24 20:30:47 +0100

    save

--- db/seeds.js
+++ db/seeds.js
@@ -10,30 +10,32 @@ const User = require('../models/user');
 Place.collection.drop();
 User.collection.drop();
 
-User
+Place
   .create([{
-    email: 'odwyercatriona@gmail.com',
-    username: 'odwyercatriona',
-    password: 'password',
-    image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKDAAAAJDliNmVlNDk1LTg1YmYtNDlkNi04OTBlLThkNmI3Yjk0ZGFlMQ.jpg'
+    name: 'Sydney',
+    image: 'https://wallscover.com/images/sydney-2.jpg',
+    whoWith: 'My best friend Laura',
+    whyThoughts: 'I always wanted to visit Sydney and see the Opera House!',
+    status: 'Been'
+  },{
+    name: 'New York',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSRuawxXHqDEkrBiwq6yuhApzQC3V6jgDK_-yxP4sryFiajrJ7_w',
+    whoWith: 'My mom',
+    whyThoughts: 'I really wanted to go shopping in New York and to see a musical!',
+    status: 'Been'
   }])
-  .then((users) => {
-    console.log(`${users.length} users created`);
-    return Place
+  .then((places) => {
+    console.log(`${places.length} places created`);
+    return User
       .create([{
-        name: 'Sydney',
-        image: 'https://wallscover.com/images/sydney-2.jpg',
-        whoWith: 'My best friend Laura',
-        whyThoughts: 'I always wanted to visit Sydney and see the Opera House!',
-        status: 'Been'
-      },{
-        name: 'New York',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSRuawxXHqDEkrBiwq6yuhApzQC3V6jgDK_-yxP4sryFiajrJ7_w',
-        whoWith: 'My mom',
-        whyThoughts: 'I really wanted to go shopping in New York and to see a musical!',
-        status: 'Been'
+        email: 'odwyercatriona@gmail.com',
+        username: 'odwyercatriona',
+        password: 'password',
+        passwordConfirmation: 'password',
+        image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKDAAAAJDliNmVlNDk1LTg1YmYtNDlkNi04OTBlLThkNmI3Yjk0ZGFlMQ.jpg',
+        places: [places[0], places[1]]
       }]);
   })
-  .then((places) => console.log(`${places.length} places been listings created`))
+  .then((users) => console.log(`${users.length} users been listings created`))
   .catch((err) => console.log(err))
   .finally(() => mongoose.connection.close());
